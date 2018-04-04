7a255cbb471619779dfb8ba85d0ed189be0431c3
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-04-04 16:24:06 +0200

    Adding fields when parsing music collection (genre, year, album, artist).

--- model/track.py
+++ model/track.py
@@ -2,11 +2,16 @@ import json
 
 
 class Track(object):
-    def __init__(self, id=None, title="", playcount=0, rating_score=0, json=None):
+    def __init__(self, id=None, title="", playcount=0, rating_score=0,
+                 artist="", genre="", album="", year=None, json=None):
         self.__id = id
         self.__title = title
         self.__playcount = playcount
         self.__rating_score = rating_score
+        self.__artist = artist
+        self.__genre = genre
+        self.__album = album
+        self.__year = year
         if json:
             self.fromJSON(json)
 
@@ -15,7 +20,11 @@ class Track(object):
             "id": self.__id,
             "title": self.__title,
             "playcount": self.__playcount,
-            "rating_score": self.__rating_score
+            "rating_score": self.__rating_score,
+            "artist": self.__artist,
+            "genre": self.__genre,
+            "album": self.__album,
+            "year": self.__year,
         })
 
     def fromJSON(self, json_line):
@@ -24,6 +33,10 @@ class Track(object):
         self.__title = entry['title']
         self.__playcount = entry['playcount']
         self.__rating_score = entry['rating_score']
+        self.__artist = entry['artist']
+        self.__genre = entry['genre']
+        self.__album = entry['album']
+        self.__year = entry['year']
 
     def __repr__(self):
         return "ID: {}, TITLE: {}, PLAYCOUNT: {}, RATING: {}\n".format(
