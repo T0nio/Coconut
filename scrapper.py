a23a56882ed81050271b6bfd4fc03565f36e17e1
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-04-04 16:24:06 +0200

    Adding fields when parsing music collection (genre, year, album, artist).

--- scrapper.py
+++ scrapper.py
@@ -28,34 +28,70 @@ if __name__ == "__main__":
                     rating = float(file['fmps_rating_amarok_score'][0])
                 except KeyError:
                     rating = None
+                try:
+                    artist = file['artist'][0]
+                except KeyError:
+                    artist = ""
+                try:
+                    genre = file['genre'][0]
+                except KeyError:
+                    genre = ""
+                try:
+                    album = file['album'][0]
+                except KeyError:
+                    album = ""
+                try:
+                    year = int(file['date'][0][:4])
+                except KeyError:
+                    year = None
 
             elif file_name.lower().endswith(('mp3')):
                 file = mutagen.File(os.path.join(dirpath, file_name))
                 try:
-                    playcount = file.tags.getall("TXXX:FMPS_PlayCount")[0]
-                    playcount = float(playcount[0])
+                    playcount = float(file.tags.getall("TXXX:FMPS_PlayCount")[0][0])
                 except IndexError:
                     playcount = None
                 try:
-                    rating = file.tags.getall("TXXX:FMPS_Rating_Amarok_Score")[0]
-                    rating = float(rating[0])
+                    rating = float(file.tags.getall("TXXX:FMPS_Rating_Amarok_Score")[0][0])
                 except IndexError:
                     rating = None
                 try:
                     title = file.tags.getall("TIT2")[0][0]
                 except IndexError:
                     title = ""
+                try:
+                    artist = file.tags.getall("TPE1")[0][0]
+                except IndexError:
+                    artist = ""
+                try:
+                    genre = file.tags.getall("TCON")[0][0]
+                except IndexError:
+                    genre = ""
+                try:
+                    album = file.tags.getall("TALB")[0][0]
+                except IndexError:
+                    album = ""
+                try:
+                    year = file.tags.getall("TDRC")[0][0]
+                    year = str(year)
+                    year = year[:4]
+                    year = int(year)
+                except (ValueError, IndexError):
+                    year = None
 
             else:
                 break  # do not treat that file
 
-            print(id, title, playcount, rating)
             track_collection[id] = Track(
                 id,
                 title,
                 playcount,
-                rating)
+                rating,
+                artist,
+                genre,
+                album,
+                year)
             id += 1
 
-    print(track_collection)
+    print("Saving track collection to : ", os.path.join('data', 'track_collection.json'))
     track_collection.save(os.path.join('data', 'track_collection.json'))
