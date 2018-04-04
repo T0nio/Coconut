15bdd0081bcb857132eae9ca4f579e5759e0b05b
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-04-04 15:35:49 +0200

    Adding mp3 metadata scrapping.

--- model/track.py
+++ model/track.py
@@ -2,18 +2,36 @@ import json
 
 
 class Track(object):
-    def __init__(self, id, title="", playcount=0, rating_score=0):
+    def __init__(self, id=None, title="", playcount=0, rating_score=0, json=None):
         self.__id = id
         self.__title = title
         self.__playcount = playcount
         self.__rating_score = rating_score
+        if json:
+            self.fromJSON(json)
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
+        return json.dumps({
+            "id": self.__id,
+            "title": self.__title,
+            "playcount": self.__playcount,
+            "rating_score": self.__rating_score
+        })
 
-    def __str__(self):
-        return "ID: {}, TITLE: {}, PLAYCOUNT: {}, RATING: {}".format(
+    def fromJSON(self, json_line):
+        entry = json.loads(json_line)
+        self.__id = entry['id']
+        self.__title = entry['title']
+        self.__playcount = entry['playcount']
+        self.__rating_score = entry['rating_score']
+
+    def __repr__(self):
+        return "ID: {}, TITLE: {}, PLAYCOUNT: {}, RATING: {}\n".format(
             self.__id,
             self.__title[:20],
             self.__playcount,
             self.__rating_score)
+
+    @property
+    def id(self):
+        return self.__id
--- scrapper.py
+++ scrapper.py
@@ -5,7 +5,6 @@ from model.track_collection import TrackCollection
 from model.track import Track
 
 ROOT_DIR_PATH = os.path.join(os.sep, 'media', 'DATA', 'Musique')
-AUTHORIZED_ZIK_EXTENSIONS = ('mp3', 'flac', 'ogg')
 
 if __name__ == "__main__":
     ext = re.compile('/\.[0-9a-z]+$/i')
@@ -15,11 +14,8 @@ if __name__ == "__main__":
         for file_name in filenames:
             if id % 100 == 0:
                 print(id)
-            print(dirpath, file_name)
-            if file_name.lower().endswith(AUTHORIZED_ZIK_EXTENSIONS):
-                # print(file_name)
+            if file_name.lower().endswith(('flac', 'ogg')):
                 file = mutagen.File(os.path.join(dirpath, file_name))
-                print(file.keys())
                 try:
                     title = file['title'][0]
                 except KeyError:
@@ -32,12 +28,34 @@ if __name__ == "__main__":
                     rating = float(file['fmps_rating_amarok_score'][0])
                 except KeyError:
                     rating = None
-                print(id, title, playcount, rating)
-                track_collection[id] = Track(
-                    id,
-                    title,
-                    playcount,
-                    rating)
-                id += 1
+
+            elif file_name.lower().endswith(('mp3')):
+                file = mutagen.File(os.path.join(dirpath, file_name))
+                try:
+                    playcount = file.tags.getall("TXXX:FMPS_PlayCount")[0]
+                    playcount = float(playcount[0])
+                except IndexError:
+                    playcount = None
+                try:
+                    rating = file.tags.getall("TXXX:FMPS_Rating_Amarok_Score")[0]
+                    rating = float(rating[0])
+                except IndexError:
+                    rating = None
+                try:
+                    title = file.tags.getall("TIT2")[0][0]
+                except IndexError:
+                    title = ""
+
+            else:
+                break  # do not treat that file
+
+            print(id, title, playcount, rating)
+            track_collection[id] = Track(
+                id,
+                title,
+                playcount,
+                rating)
+            id += 1
+
     print(track_collection)
     track_collection.save(os.path.join('data', 'track_collection.json'))
